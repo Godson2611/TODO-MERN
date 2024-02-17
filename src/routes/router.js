e2bb37de359af95089beac18040c4cc931e441e6
Godson2611 <godson2611@gmail.com>
2024-02-17 11:34:43 +0530

    Todo-Uncompleted add

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 import express from "express";
 import Todo from "./Todo.router.js"
+import User from "./User.router.js";
 
 const router = express.Router();
 
-router.use('/todos',Todo)
+router.use("/todos", Todo);
+router.use("/users", User);
 
 export default router;
